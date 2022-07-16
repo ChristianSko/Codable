5f4a87406da696125e393bafbe084fe414e3be83
Christian Skorobogatow <christiansko@icloud.com>
2022-07-16 17:36:19 +0200

    Initial Commit

---
+++ Codable/CodableApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodableApp.swift
+//  Codable
+//
+//  Created by Christian Skorobogatow on 16/7/22.
+//
+
+import SwiftUI
+
+@main
+struct CodableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
